58f7cb671cf9cf13938ecf9f345da55f2510e5fb
Joel Minaya <jminaya20@gmail.com>
2025-04-22 17:20:48 -0400

    src/Features

--- Signal/AppLaunch/AppDelegate.swift
+++ Signal/AppLaunch/AppDelegate.swift
@@ -162,8 +162,6 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         let mainAppContext = MainAppContext()
         SetCurrentAppContext(mainAppContext)
 
-        AttachmentDownloadHook.shared.install()
-
         let debugLogger = DebugLogger.shared
         debugLogger.enableTTYLoggingIfNeeded()
         DebugLogger.registerLibsignal()
