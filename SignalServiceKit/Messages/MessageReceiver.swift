2d6399300f836d00e8c1af821488e24d7d8b2ea9
Joel Minaya <jminaya20@gmail.com>
2025-05-13 23:32:15 -0400

    stuff

--- SignalServiceKit/Messages/MessageReceiver.swift
+++ SignalServiceKit/Messages/MessageReceiver.swift
@@ -5,7 +5,6 @@
 
 public import LibSignalClient
 import SignalRingRTC
-import DuplicateContentDetection
 
 /// An ObjC wrapper around UnidentifiedSenderMessageContent.ContentHint
 @objc
@@ -1149,18 +1148,7 @@ public final class MessageReceiver {
         let updatedThread = TSThread.anyFetch(uniqueId: thread.uniqueId, transaction: tx) ?? thread
 
         do {
-            // Check for duplicate content before creating attachment pointers
-            for attachmentProto in dataMessage.attachments {
-                if let attachmentData = try? attachmentProto.data {
-                    if DuplicateDetector.shared.isDuplicate(attachmentData) {
-                        Logger.info("Duplicate content detected in incoming message")
-                        // Delete the message since it contains duplicate content
-                        DependenciesBridge.shared.interactionDeleteManager.delete(message, sideEffects: .default(), tx: tx)
-                        return nil
-                    }
-                }
-            }
-
+            // Create attachment pointers
             try DependenciesBridge.shared.attachmentManager.createAttachmentPointers(
                 from: dataMessage.attachments.map { proto in
                     return .init(
@@ -1217,13 +1205,6 @@ public final class MessageReceiver {
                 )),
                 tx: tx
             )
-
-            // Store hashes of attachment data after successful creation
-            for attachmentProto in dataMessage.attachments {
-                if let attachmentData = try? attachmentProto.data {
-                    DuplicateDetector.shared.storeHash(attachmentData)
-                }
-            }
         } catch {
             owsFailDebug("Could not build attachments!")
             DependenciesBridge.shared.interactionDeleteManager
